4663743f51df33e7697bf49cceb9ce49d4c701f6
Azimjon Sobirov <sobirovazimjon02@gmail.com>
2024-10-20 05:36:00 +0300

    Commit for navigation bar in website

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import *
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/library')
+def library():
+    return "Library Page"
+
+@app.route('/create_planet')
+def create_planet():
+    return "Create Planet Page"
+
+@app.route('/register')
+def register():
+    return "Register Page"
+
+@app.route('/heroes')
+def heroes():
+    return "Heroes Page"
 
 if __name__ == '__main__':
     app.run(debug=True)
